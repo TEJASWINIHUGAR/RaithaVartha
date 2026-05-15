dd30ed09b83c85e3fb486488db4d23790221f52d
TEJASWINI I HUGAR <tejuihugar@gmail.com>
2026-05-15 10:26:31 +0000

    Add Kotlin utility module for Android layer

---
+++ android/app/src/main/java/com/raithavarta/core/AppUtils.kt
@@ -0,0 +1,20 @@
+package com.raithavarta.core
+
+class AppUtils {
+
+    fun getAppName(): String {
+        return "Raitha Varta Agriculture System"
+    }
+
+    fun getAppStatus(): String {
+        return "Active"
+    }
+
+    fun getVersion(): String {
+        return "1.0.0"
+    }
+
+    fun getModuleType(): String {
+        return "Kotlin Utility Layer"
+    }
+}
